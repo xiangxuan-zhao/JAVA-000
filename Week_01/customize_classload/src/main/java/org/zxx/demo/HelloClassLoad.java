38a6a7d9bb7fba1cf0ebb406f9259d2118c71b7d
xiangxuan <zhaoxiangxuan@123drm.com>
2020-10-22 01:02:11 +0800

    修改

--- Week_01/customize_classload/src/main/java/org/zxx/demo/HelloClassLoad.java
+++ Week_01/customize_classload/src/main/java/org/zxx/demo/HelloClassLoad.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URLClassLoader;
 
 /**
@@ -16,8 +18,12 @@ public class HelloClassLoad extends ClassLoader {
 
     public static void main(String[] args) {
         try {
-            new HelloClassLoad().findClass("Hello.xlass").newInstance();
-        } catch (InstantiationException | IllegalAccessException e) {
+           // Object hello = new HelloClassLoad().findClass("Hello").newInstance();
+            Class<?> clazz = new HelloClassLoad().findClass("Hello");
+            Object instance = clazz.getDeclaredConstructor().newInstance();
+            Method hello = clazz.getMethod("hello");
+            hello.invoke(instance);
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
             e.printStackTrace();
         }
     }
@@ -26,21 +32,22 @@ public class HelloClassLoad extends ClassLoader {
     protected Class<?> findClass(String name) {
         byte[] bytes = new byte[0];
         try {
-            bytes = getBytes(name);
+            bytes = getBytes();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return defineClass(name, bytes, 0, bytes.length);
     }
 
-    public byte[] getBytes(String name) throws IOException {
+    public byte[] getBytes() throws IOException {
 
-        File file = new File(name);
+        File file = new File("Hello.xlass");
         FileInputStream fis = new FileInputStream(file);
         byte[] bytes = new byte[(int) file.length()];
-        for(int i = 0;i < fis.read(bytes, 0, (int) file.length());i++){
-            bytes[i] = (byte)(255 - bytes[i]);
-        }
+        int n = fis.read(bytes, 0, (int) file.length());
+        for(int i = 0;i < n;i++){
+           bytes[i] = (byte)(255 - bytes[i]);
+         }
         return bytes;
     }
 }
